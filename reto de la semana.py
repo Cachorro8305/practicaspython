b1e8b774833fc590ac10dad3a6649d8fa6dd4fb9
RULAS <raulomarsancheznavarro8@gmail.com>
2023-02-09 13:33:40 -0600

    realise un ajuste

--- reto de la semana.py
+++ reto de la semana.py
@@ -1,18 +1,18 @@
 actual = int(input("Introduce el año actual. "))
 anterior = int(input("Introduce otro año para calcular. "))
-if actual > anterior:  
+if actual >= anterior:  
     print("Han pasado desde", actual - anterior," el año que as introducido")
 actual = int(input("Introduce el año actual. "))
 anterior = int(input("Introduce otro año para calcular. "))    
-if actual < anterior:
+if actual <= anterior:
     print('Faltan',anterior - actual,'años para llegar al año que as introducido')
 actual = int(input("Introduce el año actual. "))
 anterior = int(input("Introduce otro año para calcular. "))    
-if actual > anterior:
+if actual >= anterior:
     print('Desde el año',anterior, 'han pasado',actual - anterior,)
 actual = int(input("Introduce el año actual. "))
 anterior = int(input("Introduce otro año para calcular. "))    
-if actual < anterior:
+if actual <= anterior:
     print('Para llegar a',anterior,'hace falta',anterior - actual,'años')
 actual = int(input("Introduce el año actual. "))
 anterior = int(input("Introduce otro año para calcular. "))    
