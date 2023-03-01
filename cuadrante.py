f8a2aed4255c76d51151821e7038fea1c1553f81
RULAS <raulomarsancheznavarro8@gmail.com>
2023-03-01 15:01:11 -0600

    tarea 2

--- cuadrante.py
+++ cuadrante.py
@@ -1,6 +1,7 @@
-x = int(input('Ingrese cordenada de X '))
+x = int(input('Ingrese cordenada de X '))# pedimos datos a usuario
 y = int(input('Ingrese cordenada de Y '))
 
+#se calcula cuadrante mediante la condicion and
 if x > 0 and y > 0 :
     print('Punto en el primer cuadrante')
 elif x < 0 and y > 0 :
